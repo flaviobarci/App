9bbe76774d7f74ee66ecf3abeb4b632251b10ca3
SaraGrimaldi <100138563+SaraGrimaldi@users.noreply.github.com>
2022-11-22 23:59:32 +0100

    Update cypress/e2e/spec.cy.js
    Co-authored-by: Lavínia Beghini <lavinia.beghini@creditas.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
 describe('My First Test', () => {
   it('Visits our Application', () => {
     cy.visit('http://localhost:19006/')
+    cy.contains('Register').should('be.visible') // Assert that element is visible
+
   })
 })
