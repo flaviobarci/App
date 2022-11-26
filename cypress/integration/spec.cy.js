d2bc0a6b4acd8e16f5e56fcbad9df30ba4faea43
Lavínia Beghini <lavinia.beghini@creditas.com>
2022-11-27 00:58:32 +0100

    Add app preview

--- cypress/integration/spec.cy.js
+++ cypress/integration/spec.cy.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Visits our Application', () => {
-    cy.visit('http://localhost:19006/')
+    cy.visit(Cypress.env('URL'))
     cy.contains('Register').should('be.visible') // Assert that element is visible
 
   })
