940824604698e22485c1d6c56db4a7c93b1fecc0
SaraGrimaldi <100138563+SaraGrimaldi@users.noreply.github.com>
2022-12-03 14:29:30 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import RegistrationScreen from './RegistrationScreen';
+/*import RegistrationScreen from './RegistrationScreen';
 
 
 export default function App() {
@@ -6,6 +6,29 @@ export default function App() {
     <RegistrationScreen/>
   );
 }
+*/
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Signup from './src/Signup';
+import Login from './src/Login';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 
