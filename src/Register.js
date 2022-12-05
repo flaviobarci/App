c01c425f354781e65d8cc0d5f68f1c81f9112adc
unknown <20IMC10330@fh-krems.ac.at>
2022-12-05 23:39:08 +0100

    Resolves Issue #11

--- src/Register.js
+++ src/Register.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import Btn from './Btn'
 import Field from './Field'
 import { darkGreen } from './Constants'
 
 const Register = (props) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function register_api() {
+    fetch('http://127.0.0.1:8000/api/register', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password
+      })
+    });
+  }
+
+
   return (
     <View style={styles.view}>
       <Text style={styles.title}>Register</Text>
 
-      <Field placeholder="Name" />
-      <Field placeholder="Email / Username" keyboardType={'email-address'} />
-      <Field placeholder="Password" secureTextEntry={true} />
+      <Field placeholder="Name" onChangeText={(username) => setUsername(username)} />
+      <Field placeholder="Email / Username" keyboardType={'email-address'} onChangeText={(email) => setEmail(email)} />
+      <Field placeholder="Password" secureTextEntry={true} onChangeText={(password) => setPassword(password)} />
 
       <Btn
         btnLabel="Let's go!"
         Press={() => {
-          alert('Account already created')
+          register_api()
         }}
       />
       <View style={styles.form}>
