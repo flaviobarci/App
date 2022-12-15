aead07104735daeae017bb1a26d265de2c8e046f
Lavínia Beghini <lavinia.beghini@creditas.com>
2022-12-15 22:46:24 +0100

    Refactor connection to API

--- src/Register.js
+++ src/Register.js
@@ -3,89 +3,81 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import Btn from './Btn'
 import Field from './Field'
 import { darkGreen, red } from './Constants'
+import Constants from 'expo-constants';
+
 
 const Register = (props) => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMsg, setErrorMsg] = useState("");
-
-  const register_api = async () => {
-
-    await fetch('http://127.0.0.1:8000/api/register', {
+  const registerUser = async () => {
+    await fetch(`${Constants.manifest?.extra?.API_URL}`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username,
         email,
-        password
-      })
-    }).then(
-      function (response) {
-        if (response.status == 201) {
-          setErrorMsg('')
-          props.navigation.navigate('Login')
-          return
-        }
-        if (response.status==403){
-          setErrorMsg('Username/Email already taken, or invalid inputs')
-          
-        }
+        password,
+      }),
+    }).then(function (response) {
+      if (response.status === 201) {
+        setErrorMsg('')
+        props.navigation.navigate('Login')
+        return
       }
-    )
+      if (response.status === 403) {
+        setErrorMsg('Username/Email already taken, or invalid inputs')
+      }
+    })
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const uploadData = new FormData()
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const uploadData = new FormData();
-
-    const strongRegex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+    const strongRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/
 
     if (!username || username.length < 4 || username.length > 70) {
-      console.log(username)
-      alert("Invalid username.");
+      alert('Invalid username.')
     } else if (!strongRegex.test(email) || !email) {
-      alert("You did not enter a valid email.");
+      alert('You did not enter a valid email.')
     } else if (password.length < 8) {
-      alert("Password is too short.");
+      alert('Password is too short.')
     } else {
-      uploadData.append('username', username);
-      uploadData.append('email', email);
-      uploadData.append('password', password);
-      console.log(uploadData);
-      alert("Account created!");
+      uploadData.append('username', username)
+      uploadData.append('email', email)
+      uploadData.append('password', password)
+      console.log(uploadData)
+      await registerUser()
     }
-  };
+  }
 
   return (
     <View style={styles.view}>
       <Text style={styles.title}>Register</Text>
       <Text style={styles.error}>{errorMsg}</Text>
-      
-      <Field 
+
+      <Field
         placeholder="Username"
-        onChangeText={text => setUsername(text)}
+        onChangeText={(text) => setUsername(text)}
       />
       <Field
         placeholder="Email"
         keyboardType={'email-address'}
-        onChangeText={text => setEmail(text)}
+        onChangeText={(text) => setEmail(text)}
       />
       <Field
         placeholder="Password"
-        onChangeText={text => setPassword(text)}
+        onChangeText={(text) => setPassword(text)}
         secureTextEntry={true}
       />
 
-      <Btn
-        btnLabel="Let's go!"
-        Press={register_api}
-      />
+      <Btn btnLabel="Let's go!" Press={handleSubmit} />
 
       <View style={styles.form}>
         <Text style={styles.callout}>Already have an account ? </Text>
@@ -119,7 +111,7 @@ const styles = StyleSheet.create({
   },
   callout: { fontSize: 16, fontWeight: 'bold' },
   login: { color: darkGreen, fontWeight: 'bold', fontSize: 16 },
-  error: { color: red, fontSize:16 }
+  error: { color: red, fontSize: 16 },
 })
 
 export default Register
