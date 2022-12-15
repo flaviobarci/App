21a96743612f7fe79a52d7dbbfb2ef42a0e2ca36
SaraGrimaldi <100138563+SaraGrimaldi@users.noreply.github.com>
2022-12-15 11:25:29 +0100

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,96 @@
 describe('My First Test', () => {
-  it('Visits our Application', () => {
+  it('Show register', () => {
     cy.visit(Cypress.env('BASE_URL'))
-    cy.contains('Register').should('be.visible') // Assert that element is visible
+    cy.contains('UnReveal').should('be.visible')
+    cy.contains('Login').should('be.visible')
+    cy.contains('Register').should('be.visible') 
+
+    cy.contains("Register").click()
+
+    
+
+    cy.get('[placeholder="Name"]').type("Username")
+    cy.get('[placeholder="Email / Username"]').type("email@email.com")
+    cy.get('[placeholder="Password"]').type("Passwords")
+
+    cy.contains("Let's go!").click()
+
+    cy.on('windows:alert', (str) => {
+      expect(str).to.equal("Account already created")
+    })
+    
+
+    
   })
+
+  it('Show login', () => {
+    cy.visit(Cypress.env('BASE_URL'))
+    cy.contains('UnReveal').should('be.visible')
+    cy.contains('Login').should('be.visible')
+    cy.contains('Register').should('be.visible') 
+
+    cy.contains("Login").click()
+
+    
+    cy.get('[placeholder="Email"]').type("email@email.com")
+    cy.get('[placeholder="Password"]').type("Passwords")
+
+    // following there is the "Login" button test
+    cy.get('.r-borderTopLeftRadius-sv81z0 > .r-alignItems-1awozwy').click()
+
+    cy.on('windows:alert', (str) => {
+      expect(str).to.equal("Account created!")
+    })
+
+
+    
+  })
+
+  
+  it('Show failed login with wrong email', () => {
+    cy.visit(Cypress.env('BASE_URL'))
+    cy.contains('UnReveal').should('be.visible')
+    cy.contains('Login').should('be.visible')
+    cy.contains('Register').should('be.visible') 
+
+    cy.contains("Login").click()
+
+    
+    cy.get('[placeholder="Email"]').type("emailemailcom")
+    cy.get('[placeholder="Password"]').type("Passwords")
+
+    
+    cy.get('.r-borderTopLeftRadius-sv81z0 > .r-alignItems-1awozwy').click()
+
+    cy.on('windows:alert', (str) => {
+      expect(str).to.equal("Invalid email.")
+    })    
+    
+  })
+
+
+
+  it('Show failed login with wrong password', () => {
+    cy.visit(Cypress.env('BASE_URL'))
+    cy.contains('UnReveal').should('be.visible')
+    cy.contains('Login').should('be.visible')
+    cy.contains('Register').should('be.visible') 
+
+    cy.contains("Login").click()
+
+    
+    cy.get('[placeholder="Email"]').type("email@email.com")
+    cy.get('[placeholder="Password"]').type("1234")
+
+    
+    cy.get('.r-borderTopLeftRadius-sv81z0 > .r-alignItems-1awozwy').click()
+
+    cy.on('windows:alert', (str) => {
+      expect(str).to.equal("Password is too short.")
+    })    
+    
+  })
+
+
+
 })
