57b8778397019c952475040dec2cbcdccfbe914f
Lavínia Beghini <lavinia.beghini@creditas.com>
2022-12-16 22:27:42 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -2,28 +2,63 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import Btn from './Btn'
 import { darkGreen, gray, red } from './Constants'
+import Constants from 'expo-constants'
 import Field from './Field'
 
+
+
 const Login = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailErrorMsg, setEmailErrorMsg] = useState('')
-  const [passwordErrorMsg, setPasswordErrorMsg] = useState('')
 
-  const handleLogin = (event) => {
-    event.preventDefault()
+  const [ email, setEmail] = useState("");
+  const [ password, setPassword] = useState("");
+  const [ emailerrorMsg, setemailerrorMsg ] = useState("");
+  const [ passwordErrorMsg, setPassworderrorMsg ] = useState("");
 
+
+
+  const loginUser = async () => {
+    await fetch(`${Constants.manifest?.extra?.API_URL}/login`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then(function (response) {
+      if (response.status === 201) {
+        props.navigation.navigate('Login')
+        return
+      }
+      if (response.status === 403) {
+        alert('Email already taken, or invalid inputs')
+      }
+    })
+  }
+
+
+
+
+
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
     const strongRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/
 
-    setEmailErrorMsg('')
-    setPasswordErrorMsg('')
+    setemailerrorMsg('')
+    setPassworderrorMsg('')
 
     if (!strongRegex.test(email) || !email) {
-      setEmailErrorMsg('Invalid email.')
+      setemailerrorMsg('Invalid email.')
     } else if (password.length < 8) {
-      setPasswordErrorMsg('Password is too short.')
+      setPassworderrorMsg('Password is too short.')
     } else {
-      alert('Account created!')
+      await loginUser()
+
     }
   }
 
@@ -39,12 +74,19 @@ const Login = (props) => {
     <View style={styles.view}>
       <Text style={styles.title}>Login</Text>
       <Text style={styles.subtitle}>Login to your account</Text>
+
+      
+      
+      
+      <Text style = {styles.error}>{emailerrorMsg}</Text>
       <Field
         placeholder="Email"
         keyboardType={'email-address'}
         onChangeText={handleEmail}
       />
-      <Text style={styles.error}>{emailErrorMsg}</Text>
+
+
+      <Text style={styles.error}>{emailerrorMsg}</Text>
       <Field
         placeholder="Password"
         onChangeText={handlePassword}
@@ -57,6 +99,8 @@ const Login = (props) => {
         btnLabel="Let's go!"
         Press={handleLogin}
       />
+
+
       <View style={styles.form}>
         <Text style={styles.callout}>Do not have an account ? </Text>
         <TouchableOpacity onPress={() => props.navigation.navigate('Register')}>
